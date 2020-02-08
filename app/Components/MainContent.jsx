e9429034fdb5b8fe202d64882613407d5186d165
nasara249 <nasara249@gmail.com>
2020-02-08 20:34:29 +0530

    latest code

--- app/Components/MainContent.jsx
+++ app/Components/MainContent.jsx
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 function MainContent() {
     return (
-        <div>
-        Main
+        <div className="profile-view">
+            <div className="image-cont">
+                <img className="profile-img" alt="" src="../assets/img/profile.jpg" />
+            </div>
+            <div className="profile-cont">
+                <span className="profile-name">Elavarasan S</span>
+                <span className="profile-desig">Full Stack Developer</span>
+                <span className="profile-selfnote">I am a soft guy.</span>
+            </div>
         </div>
     )
 }
--- app/Layout/MainLayout.js
+++ app/Layout/MainLayout.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
 import MainContent from '../Components/MainContent';
 import About from '../Components/About';
 import { Row, Col } from 'react-bootstrap';
+import { Swipeable } from 'react-swipeable'
+
+const RIGHT = "-1";
+const LEFT = "+1";
+
+const SwipeComponent = {
+    "0": "/",
+    "1": "/about"
+};
 
 // document.addEventListener("mouseup", (e) => {
 //     if(!e.target.classList.contains("menu-toggle-button")) {
@@ -12,11 +21,39 @@ import { Row, Col } from 'react-bootstrap';
 
 class MainLayout extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMenuOpen: false,
+            componentIdx: 0
+        };
+        this.menuToggle = this.menuToggle.bind(this);
+    }
+
     menuToggle(e) {
-        !e.currentTarget.classList.contains("active") ? e.currentTarget.classList.add("active") : e.currentTarget.classList.remove("active");
+        const menuButton = document.getElementsByClassName('menu-toggle-button')[0];
+        const menuBar = document.getElementById("menuBar");
+        menuBar.classList.contains("links-container") ? menuBar.classList.remove("links-container") : menuBar.classList.add("links-container");
+        menuButton.classList.contains("active") ? menuButton.classList.remove("active") : menuButton.classList.add("active");
+        this.setState({isMenuOpen: !this.state.isMenuOpen});
+        if(e.currentTarget.hasAttribute("data-componentIdx")) {
+            this.setState({componentIdx: Number(e.currentTarget.getAttribute("data-componentIdx"))});
+        }
+    }
+
+    onSwiped(direction) {
+        const change = direction === RIGHT ? RIGHT : LEFT;
+        const adjustedIdx = this.state.componentIdx + Number(change);
+        console.log(adjustedIdx);
+        if(adjustedIdx >= 0) {
+            this.setState({ componentIdx: adjustedIdx });
+            useHistory().push(SwipeComponent[this.state.componentIdx])
+        }
+        console.log("component idx: " + this.state.componentIdx);
     }
 
     render() {
+        const { isMenuOpen } = this.state;
         return (
             <div className="main-container">
                 <Router>
@@ -25,25 +62,31 @@ class MainLayout extends Component {
                         <span></span>
                         <span></span>
                     </button>
-                    <div className="links-container">
-                        <Row className="text-center">
-                            <Col lg={3} md={4} sm={6} xs={12}>
-                                <Link to="/" className="link">Home</Link>
-                            </Col>
-                            <Col lg={3} md={4} sm={6} xs={12}>
-                                <Link to="/about" className="link">About</Link>
-                            </Col>
-                        </Row>
-                        
+                    <div id="menuBar">
+                        {isMenuOpen &&
+                        <div>
+                            <Link to="/" onClick={this.menuToggle} data-componentIdx="0" className="link">Home</Link>
+                            <Link to="/about" onClick={this.menuToggle} data-componentIdx="1" className="link">About</Link>
+                        </div>
+                        }
                     </div>
-                    <div className="main-wrapper">
-                        <Switch>
-                            <Route exact path="/">
-                                <MainContent />
-                            </Route>
-                            <Route path="/about"><About /></Route>
-                        </Switch>
-                    </div>
+                    <Swipeable
+                        trackMouse
+                        preventDefaultTouchmoveEvent
+                        onSwipedLeft={() => this.onSwiped(LEFT)}
+                        onSwipedRight={() => this.onSwiped(RIGHT)}
+                        // className="main-wrapper"
+                        style={{ height: "100%" }}
+                        >
+                        <div className="main-wrapper">
+                            <Switch>
+                                <Route exact path="/">
+                                    <MainContent />
+                                </Route>
+                                <Route path="/about"><About /></Route>
+                            </Switch>
+                        </div>
+                    </Swipeable>
                 </Router>
             </div>
         );
