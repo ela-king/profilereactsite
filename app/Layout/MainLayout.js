392a7609b0dc6b90f9a6e1d9d1fafcb513b2ae39
nasara249 <nasara249@gmail.com>
2020-02-08 21:41:53 +0530

    Update MainLayout.js

--- app/Layout/MainLayout.js
+++ app/Layout/MainLayout.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import MainContent from '../Components/MainContent';
 import About from '../Components/About';
-import { Row, Col } from 'react-bootstrap';
 import { Swipeable } from 'react-swipeable'
 
 const RIGHT = "-1";
@@ -10,7 +9,7 @@ const LEFT = "+1";
 
 const SwipeComponent = {
     "0": "/",
-    "1": "/about"
+    "1": "about"
 };
 
 // document.addEventListener("mouseup", (e) => {
@@ -30,14 +29,14 @@ class MainLayout extends Component {
         this.menuToggle = this.menuToggle.bind(this);
     }
 
-    menuToggle(e) {
+    menuToggle(e, idx) {
         const menuButton = document.getElementsByClassName('menu-toggle-button')[0];
         const menuBar = document.getElementById("menuBar");
         menuBar.classList.contains("links-container") ? menuBar.classList.remove("links-container") : menuBar.classList.add("links-container");
         menuButton.classList.contains("active") ? menuButton.classList.remove("active") : menuButton.classList.add("active");
         this.setState({isMenuOpen: !this.state.isMenuOpen});
-        if(e.currentTarget.hasAttribute("data-componentIdx")) {
-            this.setState({componentIdx: Number(e.currentTarget.getAttribute("data-componentIdx"))});
+        if(idx) {
+            this.setState({componentIdx: Number(idx)});
         }
     }
 
@@ -45,9 +44,9 @@ class MainLayout extends Component {
         const change = direction === RIGHT ? RIGHT : LEFT;
         const adjustedIdx = this.state.componentIdx + Number(change);
         console.log(adjustedIdx);
-        if(adjustedIdx >= 0) {
+        if(adjustedIdx >= 0 && Object.keys(SwipeComponent).length+1 >= adjustedIdx) {
             this.setState({ componentIdx: adjustedIdx });
-            useHistory().push(SwipeComponent[this.state.componentIdx])
+            window.location.href = SwipeComponent[this.state.componentIdx];
         }
         console.log("component idx: " + this.state.componentIdx);
     }
@@ -57,7 +56,7 @@ class MainLayout extends Component {
         return (
             <div className="main-container">
                 <Router>
-                    <button className="menu-toggle-button" onClick={this.menuToggle}>
+                    <button className="menu-toggle-button" onClick={(event) => this.menuToggle(event)}>
                         <span></span>
                         <span></span>
                         <span></span>
@@ -65,8 +64,8 @@ class MainLayout extends Component {
                     <div id="menuBar">
                         {isMenuOpen &&
                         <div>
-                            <Link to="/" onClick={this.menuToggle} data-componentIdx="0" className="link">Home</Link>
-                            <Link to="/about" onClick={this.menuToggle} data-componentIdx="1" className="link">About</Link>
+                            <Link to="/" onClick={(event) => this.menuToggle(event, "0")} className="link">Home</Link>
+                            <Link to="/about" onClick={(event) => this.menuToggle(event, "1")} className="link">About</Link>
                         </div>
                         }
                     </div>
